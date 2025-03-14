a724b50cedb0e86f1d1759d63b9387b6b81e844d
Yuya Kusaka <10cherry07@gmail.com>
2025-03-14 09:30:04 +0900

    Solved 46th problem (Medium)

--- LeetCode75/46.py
+++ LeetCode75/46.py
@@ -1,37 +1,32 @@
 class Solution:
-    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        answers = dict()
-        exists = dict()
+    def dfs(self, candidates, equation, v):
+        for (c, d), val in list(candidates.items()):
+            a, b = equation                        
+            if b == c:                
+                candidates[(a, d)] = v * val
+                candidates[(d, a)] = 1 / candidates[(a, d)]
+            if b == d:
+                candidates[(a, c)] = v * (1 / val)                
+                candidates[(c, a)] = 1 / candidates[(a, c)]
+    
+    def calcEquation(self, equations, values, queries):
+        candidates = dict()
         for i in range(len(equations)):
-            a, b = equations[i][0], equations[i][1]
-            answers[(a, b)] = values[i]
-            answers[(b, a)] = 1 / values[i]
-            exists[a] = b
-            exists[b] = a
-        print(answers)
-        for i in range(len(equations)):
-            for j in range(len(equations)):
-                a, b = equations[i][0], equations[i][1]
-                c, d = equations[j][0], equations[j][1]
-                if a not in exists or b not in exists or c not in exists or d not in exists:
-                    continue
-                print(f'a = {a}, b = {b}, c = {c}, d = {d}')
-                if a == c:
-                    answers[(b, d)] = answers[(b, a)] * answers[(c, d)]
-                    answers[(d, b)] = 1 / answers[(b, d)]
-                if a == d:
-                    answers[(b, c)] = answers[(b, a)] * answers[(d, c)]
-                    answers[(c, b)] = 1 / answers[(b, c)]
-                if b == c:
-                    answers[(a, d)] = answers[(a, b)] * answers[(c, d)]
-                    answers[(d, a)] = 1 / answers[(a, d)]
-                if b == d:
-                    answers[(a, c)] = answers[(a, b)] * (1 / answers[(c, d)])
-                    answers[(c, a)] = 1 / answers[(a, c)]
+            a, b = equations[i]
+            value = values[i]
+            candidates[(a, b)] = value
+            candidates[(b, a)] = 1 / value            
+            
         ans = []
+        while True:
+            n = len(candidates)
+            for (a, b), value in list(candidates.items()):                
+                self.dfs(candidates, (a, b), value)
+            if n == len(candidates):
+                break
         for a, b in queries:
-            if (a, b) in answers:
-               ans.append(answers[(a,b)])
+            if (a, b) not in candidates:
+                ans.append(-1)
             else:
-                ans.append(-1.0)
+                ans.append(candidates[(a, b)])
         return ans
