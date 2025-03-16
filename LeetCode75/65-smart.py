33e727019ede3fa0860f36406a5cf2e2c266e042
Yuya Kusaka <10cherry07@gmail.com>
2025-03-16 12:12:54 +0900

    Solved 65th problem (Medium)

--- LeetCode75/65-smart.py
+++ LeetCode75/65-smart.py
@@ -1,23 +1,24 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         memo = {}
+
         def dfs(index, buy):
-            if index>=len(prices):
+            if index >= len(prices):
                 return 0
-            
+
             if (index, buy) in memo:
                 return memo[(index, buy)]
-            
+
             res = 0
             if buy:
-                yb = dfs(index+1, not buy)-prices[index]
-                nb = dfs(index+1, buy)
+                yb = dfs(index + 1, not buy) - prices[index]
+                nb = dfs(index + 1, buy)
                 res = max(yb, nb)
             else:
-                ys = dfs(index+1, not buy)+prices[index]-fee
-                ns = dfs(index+1, buy)
+                ys = dfs(index + 1, not buy) + prices[index] - fee
+                ns = dfs(index + 1, buy)
                 res = max(ys, ns)
-            memo[(index,buy)] = res
+            memo[(index, buy)] = res
             return res
-        return dfs(0,True)
-                
+
+        return dfs(0, True)
---
+++ LeetCode75/65.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        memo = {}
+        def dfs(index, buy):
+            if index >= len(prices):
+                return 0
+            if (index, buy) in memo:
+                return memo[(index, buy)]   
+            res = 0
+            if buy:
+                by = dfs(index + 1, not buy) - prices[index]
+                bn = dfs(index + 1, buy)
+                res = max(by, bn)
+            else:
+                sy = dfs(index + 1, not buy) + prices[index] - fee
+                sn = dfs(index + 1, buy)
+                res = max(sy, sn)
+            memo[(index, buy)] = res
+            return res
+        return dfs(0, True)
